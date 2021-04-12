7fab2556aefdb29f081755fe83ab9e01dfe3dbd6
hyjoong <maplesosser@naver.com>
2021-04-12 16:32:01 +0900

    Add: 초기세팅 완료

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,11 @@ export const theme = {
   white: '#FFFFFF',
   blue: '#4360FF',
   black: '#000000',
-
+  browny: '#292929',
+  gray: '#E9E9E9',
+  darkBlack: '#2F2F2F',
+  brown: '#b7956e',
+  deepBrown: '#b97334',
+  lightBrown: '#c0a388',
   typo: FONT_SIZE,
 } as const;
